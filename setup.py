850c00837da17240ab1e52ab69ca0359928b5cee
Arun Kumar <arun@coverfoxmail.com>
2016-07-09 12:21:57 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-manage-reset-migrations',
+    version='beta',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GNU GENERAL PUBLIC LICENSE',
+    description='A management utility to reset migrations',
+    long_description=README,
+    url='https://github.com/arun6582/django-manage-reset-migrations/',
+    author='Arun Kumar',
+    author_email='arun6582@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
