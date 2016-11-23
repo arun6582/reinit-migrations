6b713c79f43ad1c7a04c9bdedca63850b4aa90d3
Arun Kumar <arun6582@gmail.com>
2016-11-23 19:31:49 +0530

    changes

--- reinit_migrations/apps.py
+++ reinit_migrations/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class ResetInitManageCommandConfig(AppConfig):
+class ReInitMigrationsConfig(AppConfig):
     name = 'reinit_migrations'
